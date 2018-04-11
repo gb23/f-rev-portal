4c2e2d074e00943fd769b907424306c857688595
gb23 <gregbenj@hotmail.com>
2018-04-11 13:22:45 -0700

    add store, provider to app

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import './index.css';
-import App from './App';
+//import './index.css';
+import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
+import store from './store.js';
 import 'font-awesome/css/font-awesome.min.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+    
+);
 registerServiceWorker();
