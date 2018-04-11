c0f88a3482abf826f5a68981f5be57dac76529cb
gb23 <gregbenj@hotmail.com>
2018-04-11 13:22:19 -0700

    create store

---
+++ src/store.js
@@ -0,0 +1,27 @@
+import { createStore,applyMiddleware,combineReducers } from 'redux'
+import thunk from 'redux-thunk';
+//import sections from './reducers/sections';
+//import sectionFormData from './reducers/sectionFormData';
+//import sectionCurrent from './reducers/sectionCurrent';
+//import composition from './reducers/composition';
+import {  routerMiddleware} from 'react-router-redux'
+
+import createHistory from 'history/createBrowserHistory'
+
+export const history = createHistory()
+const middleware = [thunk, routerMiddleware(history)];
+
+const reducers = combineReducers({
+    //sections,
+   // sectionFormData,
+    //sectionCurrent,
+    //composition,
+   
+})
+const store = createStore(
+    reducers,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    applyMiddleware(...middleware)
+);
+
+export default store;
