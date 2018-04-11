6e54637db078007a38a67e13b29185fac42cf972
gb23 <gregbenj@hotmail.com>
2018-04-11 14:50:02 -0700

    route landing page to '/businesses'  abd render businesses component

---
+++ src/containers/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import {Route, Redirect, Switch, BrowserRouter as Router} from 'react-router-dom'
+//???import { ConnectedRouter } from 'react-router-redux'
+//import logo from '../logo.svg';
+////import './App.css';
+import NotFound from '../components/NotFound'
+//??import { history } from '../store.js'
+import Businesses from './Businesses.js';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+      {/* // <ConnectedRouter history={history}> */}
+      <div className="App">
+      
+        <Switch>
+          <Route exact path="/" render={() => <Redirect to="/businesses" />} />
+          {/* <Route exact path="/compositions" render={() => <Redirect to="/compositions/:id" />} /> */}
+          <Route exact path="/businesses" component={Businesses} />
+                        
+          <Route path="*" component={NotFound} />
+        </Switch> 
+      </div>
+           {/* // <ConnectedRouter  */}
+      </Router>
+    );
+  }
+}
+
+export default App;
