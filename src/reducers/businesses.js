fc30de206a7ef76e01965405f21698453236c04f
gb23 <gregbenj@hotmail.com>
2018-04-12 10:05:32 -0700

    add filter to reducer

--- src/reducers/businesses.js
+++ src/reducers/businesses.js
@@ -1,7 +1,8 @@
 export default (state = {businesses: null}, action) => {
     //debugger;
    if(action.type !== "FILTER_FRANCHISE" && action.type !== "FILTER_STATUS" 
-    && action.type !== "SELECT_BUSINESS" && action.type !== "GET_SUBSCRIBERS_SUCCESS"){
+    && action.type !== "SELECT_BUSINESS" && action.type !== "GET_SUBSCRIBERS_SUCCESS"
+    && action.type !== "ADD_NAME" && action.type !== "ADD_EMAIL" && action.type !== "LOADING"){
         switch(action.type ){
             case 'GET_BUSINESSES_SUCCESS':
                 return [...action.businesses.businesses] //this object has key 'businesses'
