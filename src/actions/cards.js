d0c45e00e8c520e04ce6f7b6eb2c41c56f75e8af
gb23 <gregbenj@hotmail.com>
2018-04-12 05:19:41 -0700

    comment out code

---
+++ src/actions/cards.js
@@ -0,0 +1,30 @@
+// import React from 'react';
+// import Card from '../components/Card';
+
+
+// export const makeArrayOfCards = elements => {
+//     return elements.map(element => 
+//         <Card key={element.id}
+//             name={element.name}
+//             cents={element.revenue_to_date_in_cents}
+//             franchise={element.franchise} 
+//             status={element.status}
+//             onChoose={onCardSelect}
+//             id = {element.id}
+//         />
+//     );
+// }
+
+// const onCardSelect = (event, id) => {
+    
+    
+// }
+
+
+// export const siftCards = (elements, filter, keyword)=>{
+//     let eles = [];
+//     if (filter !== ""){
+//         eles = elements.filter(ele => ele[keyword] === filter);
+//     }
+//     return makeArrayOfCards(eles);
+// }
