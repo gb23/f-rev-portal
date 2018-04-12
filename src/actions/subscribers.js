b9b8199489bc9a88f799c9c4209218c01746eb6d
gb23 <gregbenj@hotmail.com>
2018-04-12 10:03:49 -0700

    add action creators

--- src/actions/subscribers.js
+++ src/actions/subscribers.js
@@ -10,6 +10,23 @@ const setSubscribers = subObj => { //make loading true here
     }
 }
 
+export const addName = name => {
+    return {
+        type: 'ADD_NAME',
+        name
+    }
+}
+export const addEmail = email => {
+    return {
+        type: 'ADD_EMAIL',
+        email
+    }
+}
+const loading = () => {
+    return{
+        type: 'LOADING'
+    }
+}
 
 
 //***ASYNC ACTIONS ***
@@ -21,5 +38,29 @@ export const getSubscribers = (id) => {
             dispatch(setSubscribers(subscribers))
         })
         .catch(error => console.log(error));
+    }
+}
+export const createSubscriber = (subscriber, id) => {
+    //debugger;
+    const bodyObj = {"subscriber": subscriber}
+    //debugger;
+    return dispatch => {
+        return fetch(`${API_URL}/businesses/${id}/subscribers?${token}`, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(bodyObj)
+        })
+            .then(response => {
+                debugger;
+                response.json()})
+            .then(subscriber => {
+                console.log("logging subscriber response", subscriber);
+            })
+            .catch(error => {
+                debugger;
+            });
+        
     }
 }
