87363d06555af023b67a776cd355cb28e3371573
gb23 <gregbenj@hotmail.com>
2018-04-12 05:16:45 -0700

    import index.css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-//import './index.css';
+import './css/index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 import store from './store.js';
