c9265133406f22fe91c4ef067a3067c27ecbfbef
gb23 <gregbenj@hotmail.com>
2018-04-12 18:19:05 -0700

    add style

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,17 +4,26 @@ const Form = (props) => {
     //debugger;
     return (
         <div
-            className="fixRightMost"   
+            className="fixRightMost ba b--black-10"   
         >
-            <label>Add Subscriber to selected Business</label>
+            <span>Want to Add Subscriber to </span> <span className="highlight"> Selected Business </span>
+            <span>?</span>
             <form onSubmit={props.handleOnSubmit}>
-                <label htmlFor="name">Full Name</label>
-                <input type="text" name="name" onChange={(event) => props.handleInputChange(event)} value={props.name}/>
-                <br/>
-                <label htmlFor="email">Email</label>
-                <input type="email" name="email"onChange={(event) => props.handleInputChange(event)} value={props.email}/>
-                <br/>
-                <button type="submit">Add Subscriber</button>
+            <div className="mt3">
+                <label className="dbfw4 lh-copy f6" htmlFor="name">Full Name</label>
+                <input className="pa2 input-reset ba bg-transparent w-100 measure" type="text" name="name" onChange={(event) => props.handleInputChange(event)} value={props.name}/>
+            </div>
+            <div className="mt3">
+                <label className="dbfw4 lh-copy f6" htmlFor="email">Email</label>
+                <input className="pa2 input-reset ba bg-transparent w-100 measure" type="email" name="email"onChange={(event) => props.handleInputChange(event)} value={props.email}/>
+            </div>
+            <div className="mt3"> 
+                {props.validate() ? <input className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6" type="submit" value="Add Subscriber"/ > :
+                <div className="b ph3 pv2 ba b--black bg-transparent grow f6">
+                <span className="center rightMove"> Provide Valid Information </span>
+                </div> }
+             
+            </div>
             </form>
         </div>
     );
