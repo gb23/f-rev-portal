4f89bb1b4787c6ce1a5aebb9ec78613a50d53a2f
gb23 <gregbenj@hotmail.com>
2018-04-12 18:18:38 -0700

    format text

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,14 +4,14 @@ const Card = (props) => {
     //debugger;
     return (
         <div tabIndex="0"
-            className=""
+            className={`card pl3 ba bt b--black-10 ${props.highlight}`}
             onClick={ (event) => props.onChoose(event, props.id) }
         >
             <h3>{props.name}</h3>
-            <h5>{props.email ? `Email: ${props.email}` : ""}</h5>
-            <h5>{props.cents ? `Revenue In Cents: ${props.cents}` : ""}</h5>
-            <h5>{props.franchise ? `Franchise: ${props.franchise}` : ""}</h5>
-            <h5>{props.status ? `Status: ${props.status}` : ""}</h5>
+            <p className="mb0 mt1">{props.email ? `Email: ${props.email}` : ""}</p>
+            <p className="mb0 mt1">{props.cents ? `Revenue In Cents: ${props.cents}` : ""}</p>
+            <p className="mb0 mt1">{props.franchise ? `Franchise: ${props.franchise}` : ""}</p>
+            <p className="mb0 mt1">{props.status ? `Status: ${props.status}` : ""}</p>
 
             <br/>
         </div>
